546b312afd83c63b93b8c340f43e2cc2a4d15065
teymuur <teymur_babayev08@yahoo.com>
2024-03-30 01:40:55 +0400

    Final Update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-window.onload = function() {
-    const links = document.querySelectorAll('.link');
-    const iframe = document.getElementById('iframe-container');
-  
-    links.forEach(link => {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        links.forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
-        const src = this.getAttribute('href');
-        iframe.setAttribute('src', src);
-      });
-    });
-  }
+document.getElementById('level1').addEventListener('click', function() {
+  document.getElementById('iframe-container').src = 'https://images.nationalgeographic.com/wpf/media-content/richmedia/1/1143/project/dist/desktop.html';
+  document.getElementById('link').href = 'https://images.nationalgeographic.com/wpf/media-content/richmedia/1/1143/project/dist/desktop.html';
+});
+
+document.getElementById('level2').addEventListener('click', function() {
+  document.getElementById('iframe-container').src = 'https://scratch.mit.edu/projects/16960241/embed';
+  document.getElementById('link').href = 'https://scratch.mit.edu/projects/16960241/embed';
+});
+
+document.getElementById('level3').addEventListener('click', function() {
+  document.getElementById('iframe-container').src = 'ttps://wordwall.net/embed/45503fdf1d5b4c50a92103a6fc389bf9?themeId=21&templateId=69&fontStackId=0'; // Replace with the actual URL for level 3
+  document.getElementById('link').href = 'ttps://wordwall.net/embed/45503fdf1d5b4c50a92103a6fc389bf9?themeId=21&templateId=69&fontStackId=0'; // Replace with the actual URL for level 3
+});
